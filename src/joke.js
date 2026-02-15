574e61511bfc5c650926044087eebb4a91d90d33
nagmanijobs <nagmanijobs@gmail.com>
2026-02-15 04:50:32 +0000

    Add ncc build script and bundled dist/index.js

--- src/joke.js
+++ src/joke.js
@@ -1,18 +1,12 @@
-const request = require("request-promise");
-
-const options = {
-  method: "GET",
-  uri: "https://icanhazdadjoke.com/",
-  headers: {
-    Accept: "application/json",
-    "User-Agent": "Writing JavaScript action GitHub Skills exercise.",
-  },
-  json: true,
-};
-
 async function getJoke() {
-  const res = await request(options);
-  return res.joke;
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "Writing JavaScript action GitHub Skills exercise.",
+    },
+  });
+  const data = await response.json();
+  return data.joke;
 }
 
 module.exports = getJoke;
